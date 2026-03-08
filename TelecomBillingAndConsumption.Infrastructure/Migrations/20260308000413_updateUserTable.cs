3b29cd09da738b4e093d080cc6a3f327fbbea8a4
Guirguis <ephraimhedia2@gmail.com>
2026-03-08 02:25:35 +0200

    UpdateWIthMigrations

---
+++ TelecomBillingAndConsumption.Infrastructure/Migrations/20260308000413_updateUserTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelecomBillingAndConsumption.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPaid",
+                table: "Bills",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPaid",
+                table: "Bills",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
